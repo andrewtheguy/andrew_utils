55413acbe547d459544175ef78a8eccb984870d6
andrew <hello@andrewtheguy.com>
2026-04-23 05:19:05 +0000

    refactor: remove obsolete test files for get_md5sum_file function

--- tests/test_md5sum.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-from andrew_utils import get_md5sum_file
-
-def test_file_md5sum():
-    dir = os.path.dirname(os.path.realpath(__file__))
-    assert get_md5sum_file(os.path.join(dir,'test.bin')) == "b3e8dd762f95e0066de2c9c66c35a24a"
